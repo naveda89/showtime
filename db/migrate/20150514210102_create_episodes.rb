f17e5a866df0aa2e358edbb88931abe3c2ca8e2f
Alvaro Naveda <naveda89@gmail.com>
2015-05-15 21:08:14 +0200

    Fixed create episodes migration

--- db/migrate/20150514210102_create_episodes.rb
+++ db/migrate/20150514210102_create_episodes.rb
@@ -1,7 +1,7 @@
 class CreateEpisodes < ActiveRecord::Migration
   def change
     create_table :episodes do |t|
-      t.references :season, index: true, foreign_key: true
+      t.references :season
       t.integer :num
 
       t.timestamps null: false
