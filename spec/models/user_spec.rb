4f49a4c7b6b8e3e53356b4042c21d7ba8b5ea4d2
Alvaro Naveda <naveda89@gmail.com>
2015-05-15 21:19:45 +0200

    Add User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Validations
+  it 'has a valid factory' do
+    user = create(:user)
+    expect(user).to be_valid
+  end
+
 end
