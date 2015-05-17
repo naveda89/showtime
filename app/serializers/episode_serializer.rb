ea29f1367967edc87b969988acf97060d8415a40
Alvaro Naveda <naveda89@gmail.com>
2015-05-17 17:25:52 +0200

    Episodes improvements (serializer & specs)

--- app/serializers/episode_serializer.rb
+++ app/serializers/episode_serializer.rb
@@ -1,3 +1,7 @@
 class EpisodeSerializer < ActiveModel::Serializer
-  attributes :id, :num, :plot
+  attributes :id, :num, :plot, :url
+
+  def url
+    api_v1_season_episode_path(object.season, object)
+  end
 end
