50af15023e8f21de78a4309afd32dcf379963b75
Alvaro Naveda <naveda89@gmail.com>
2015-05-18 01:35:00 +0200

    Remove ApiController specs and restrict TODO

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,13 +1,7 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      before_filter :restrict_access
       respond_to :json
-
-      private
-      def restrict_access
-        # TODO: Apply some restrict access policy
-      end
     end
   end
 end
--- spec/controllers/api/v1/api_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Api::V1::ApiController, type: :controller do
-
-  it 'restrict access'
-
-end
